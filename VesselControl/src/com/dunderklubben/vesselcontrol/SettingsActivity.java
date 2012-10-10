8e067d1ee4820c654635d7423450165b13eb11d2
Rasmus Jansson <rallejansson37@gmail.com>
2012-10-10 18:13:57 +0200

    minor tweaks

--- VesselControl/src/com/dunderklubben/vesselcontrol/SettingsActivity.java
+++ VesselControl/src/com/dunderklubben/vesselcontrol/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.dunderklubben.vesselcontrol;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
+import android.text.InputType;
 
 public class SettingsActivity extends PreferenceActivity  {
 
@@ -9,5 +11,9 @@ public class SettingsActivity extends PreferenceActivity  {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        
+        //Set input type of the threshold preference to number 
+        EditTextPreference pref = (EditTextPreference)findPreference("threshold");
+        pref.getEditText().setInputType(InputType.TYPE_CLASS_NUMBER);
     }
 }
