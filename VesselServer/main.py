81c1e65e68e326d632cdc77154ac8ed0405d9deb
Joar Svensson <joar@lmbmx.com>
2012-10-10 12:11:55 +0200

    Updated comments and defined variables
    Updated comments and defined variables for serial connection and socket
    server.

--- VesselServer/main.py
+++ VesselServer/main.py
@@ -2,9 +2,16 @@
 from serial import Serial
 import SocketServer
 
-ser = Serial('COM4', 9600, timeout=1)
+# Define serial settings
+port = 'COM4'
+baudrate = 9600
 
-print("Connected to: " + ser.portstr)
+# Initiate serial connection
+ser = Serial(port, baudrate, timeout=1)
+
+print("Server connected to: " + ser.portstr)
+
+# This function sends instructions to the Arduino
 def move(servo, angle):
     if (0 <= angle <= 180):
         ser.write(chr(255))
@@ -13,9 +20,13 @@ def move(servo, angle):
         ser.flush()
         print("Command sent to servo "  + str(servo) + " angle = " +str(angle))
     else:
-        print "Servo angle must be an integer between 0 and 180. now it's " + str(angle)+ "\n"
+        print "Servo angle must be an integer between 0 and 180. Now it's " + str(angle)+ "\n"
+        
 
+# This class contains the socket server
 class MyTCPHandler(SocketServer.BaseRequestHandler):
+    
+    # Function to handle connections and sending instructions to the Arduino
     def handle(self):        
         print("Client connected")
         while(True):
@@ -29,9 +40,17 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
                 if(servo > 0 and servo< 4):
                     move(servo, angle)
 
-server = SocketServer.TCPServer(("0.0.0.0", 1000), MyTCPHandler)
+# Define socket server settings
+socketAddress = '0.0.0.0'
+socketPort = 10000
+
+# Initiate socket server
+server = SocketServer.TCPServer((socketAddress, socketPort), MyTCPHandler)
 server.serve_forever()
 
+# Closing serial connection
 ser.close()
+
+# Closing socket connection
 server.close_request(MyTCPHandler)
 print("Connection closed")
