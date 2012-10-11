10fab443389d60aca30837620ba0806b2c480ab6
Joar Svensson <joar@lmbmx.com>
2012-10-11 12:35:11 +0200

    Added serial write support
    Added support for writing serial to python server.

--- VesselServerArduino/VesselServerArduino.ino
+++ VesselServerArduino/VesselServerArduino.ino
@@ -17,20 +17,21 @@ void setup()
   servoX.attach(9);
   servoY.attach(10);
   Serial.begin(9600);
+  Serial.println("Arduino available");
 }
 
 void loop() 
 { 
     if(Serial.available() >= 3)
     {
-	  int data = 0;
+      int data = 0;
       data = Serial.read(); 
       if(data == 255)
       {
         int servo = Serial.read();
         int angle = Serial.read();  
-        moveServo(servo, angle); 
-        Serial.println("Test");   
+        moveServo(servo, angle);
+        Serial.println("Arduino response");   
       }
     }
 }
