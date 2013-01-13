a040754ed8f2e88bbb9002c41862b90f9ed1f221
Joar Svensson <joar@lmbmx.com>
2013-01-13 18:53:04 +0100

    Added support for Pololu motorshield

--- VesselServerArduino/VesselServerArduino.ino
+++ VesselServerArduino/VesselServerArduino.ino
@@ -6,26 +6,20 @@
 */
 
 #include <Servo.h>
-#include <AFMotor.h>
+#include "DualVNH5019MotorShield.h"
 
-// Initialize servos and motors
+// Initialize servos
 Servo servoX;
 Servo servoY;
-//AF_DCMotor motor1(1);
-//AF_DCMotor motor2(2);
-//AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
+
+// Initialize motorshield
+DualVNH5019MotorShield md;
 
 void setup() 
 {
   servoX.attach(9);
   servoY.attach(10);
-  
-  motor4.run(RELEASE);
-  //mainMotor1.run(RELEASE);
-  //mainMotor2.run(RELEASE);
-  //bowThruster.run(RELEASE);
-  //sternThruster.run(RELEASE);
+  md.init();
   
   Serial.begin(9600);
   Serial.println("Arduino Uno available");
@@ -40,9 +34,32 @@ void loop()
       if(id >=1 && id <=6)   {
         int value = Serial.read();
         int dir = Serial.read();
-        moveServo(id, value, dir); 
+        moveServo(id, value, dir);
+        
+        // Motor 1, -400 to 400. Zero is full throttle.
+        md.setM1Speed(value);
+        stopIfFault();
+        
+        // Motor 2
+        md.setM2Speed(value);
+        stopIfFault();
       }
     }
+}
+
+// Stop motors if fault arise
+void stopIfFault()
+{
+  if (md.getM1Fault())
+  {
+    Serial.println("M1 fault");
+    while(1);
+  }
+  if (md.getM2Fault())
+  {
+    Serial.println("M2 fault");
+    while(1);
+  }
 }
 
 // Method for moving servo
