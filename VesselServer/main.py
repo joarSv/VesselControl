d087417adf16ee1f4ec656582ce3b5fe426d54f5
Joar <joar@lmbmx.com>
2014-03-04 18:09:10 +0100

    New logging feature

--- VesselServer/main.py
+++ VesselServer/main.py
@@ -1,95 +1,82 @@
 #
 #    VesselServer
-#        Created 2012-10-09
-#        Rasmus Jansson and Joar Svensson
-#        Version 0.2
-#	 https://github.com/JoarSvensson/VesselControl.git
-#        
+#    	Created 2013-06-29
+#     Rasmus Jansson and Joar Svensson
+#     Version 0.2
+#	 		https://github.com/JoarSvensson/VesselControl.git
+#       
 
+import SocketServer, os, time, threading, thread, logging
 from serial import Serial
-import SocketServer, os, time
-#import RPi.GPIO as GPIO
+from socket import *
 
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(11, GPIO.OUT)
-        
-# This class contains the socket server
-class VesselTCPHandler(SocketServer.BaseRequestHandler):
-    
-    # Function to handle connections and sending instructions to the Arduino
-    def handle(self):       
+''' This function handles connections '''
+def handler(clientsock,addr):	
 	connected = True
-	printLog(str(time.time()) + ': Client connected')
-        
+
 	while(connected):
-            self.data = self.request.recv(3)
-            if(self.data == ""):		
-		printLog(str(time.time()) + ': Client disconnected')
-                break
-            if(len(self.data) == 3):
-                number = ord(self.data[0])
-                value = ord(self.data[1])
-                direction = ord(self.data[2])
-                if(number > 0 and number < 7):
-                    move(number, value, direction)
-                    
-        connected = False
-        # Stop motors when client disconnect
-	printLog(str(time.time()) + ': Motors de-attached')
-        move(1,90,0)
-        move(3,100,0)
-        move(4,100,0)
-        
+		data = clientsock.recv(3)
+		if(data == ""):
+			print 'Client disconnected from ' + str(addr)
+			logging.info('Client disconnected from '+ str(addr))
+			break
+			
+		if(len(data) == 3):
+			number = ord(data[0])
+			value = ord(data[1])
+			direction = ord(data[2])
+			if(number > 0 and number < 7):
+				move(number, value, direction)
+			
+	connected = False
+	# Stop motors when client disconnect
+	logging.info('Motors de-attached and stopped')
+	move(1,90,0)
+	move(3,100,0)
+	move(4,100,0)
+	clientsock.close()
+
 # This function sends instructions to the Arduino Uno
 def move(number, value, direction):
-        ser.write(chr(number))
-        ser.write(chr(value))
-        ser.write(chr(direction))
-        ser.flush()	
-	printLog(str(time.time()) + ': Moving ' + str(number) + ' to ' + str(value))
+	ser.write(chr(number))
+	ser.write(chr(value))
+	ser.write(chr(direction))
+	ser.flush()	
+	logging.info('Moving ' + str(number) + ' to ' + str(value))
 
-# This function writes messages to log file
-def printLog(message):
-	f.write(message + '\n')
-	f.flush()
-        
-#def shutDown():
-#    if GPIO.input(11): 
-#        os.system("poweroff")
 
-# Initiate log file    
-f = open('VesselServer.log','w')
+''' Run server and listen for incoming client connections'''
+if __name__=='__main__':
+	# Initiate logging
+	logging.basicConfig(format='%(asctime)s %(message)s',filename='VesselServer.log',level=logging.DEBUG)
+	
+	# Settings variables
+	socketAddress = '0.0.0.0'
+	socketPort = 10000
+	port = '/dev/ttyACM0'
+	baudrate = 9600
+	connected = False
 
-# Socket server settings
-socketAddress = '0.0.0.0'
-socketPort = 10000
-        
-# Serial settings for Arduino Uno
-# Linux: /dev/ttyACM0
-# Windows: COM1
-port = '/dev/ttyACM0'#'COM4'
-baudrate = 9600
-connected = False
+	# Initiate serial connections
+	try:
+		ser = Serial(port, baudrate, timeout=1)
+		logging.info('Server connected to Arduino on: ' + str(ser.portstr))
+	    
+	except Exception, error:
+		logging.warning('Connection with Arduino failed: ' + str(error))
 
-# Initiate serial connections
-try:
-    ser = Serial(port, baudrate, timeout=1)
-    printLog(str(time.time()) + ': Server connected to:' + str(ser.portstr))
-    
-except Exception, error:
-    printLog(str(time.time()) + ': Error, connection with Arduino failed: ' + str(error))
-    
+	ADDR = (socketAddress, socketPort)
+	serversock = socket(AF_INET, SOCK_STREAM)
+	serversock.bind(ADDR)
+	serversock.listen(2)
+	
+	print 'Server started on port '+ str(socketPort) +', waiting for connections...'
+	logging.info('Server started on port '+ str(socketPort) +', waiting for connections...')
 
-# Initiate socket server
-server = SocketServer.TCPServer((socketAddress, socketPort), VesselTCPHandler)
-server.serve_forever()
+	# Run server and listen for connections
+	while True:
+		clientsock, addr = serversock.accept()		
+		print 'Client connected from: ', addr
+		logging.info('Client connected from: ' + str(addr))
+		thread.start_new_thread(handler, (clientsock, addr))
 
-# Close serial connections
-ser.close()
-
-# Close socket connection
-server.close_request(VesselTCPHandler)
-
-# Print to log
-printLog(str(time.time()) + ': Connection closed\n-----------------------------------------------------------')
-f.close()
